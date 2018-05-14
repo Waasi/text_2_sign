f125065c283e7f24b445dadf758f150e4f1e536d
Eric Santos <eric.leinad92@gmail.com>
2018-05-13 23:38:53 -0400

    [Refactor] Performance & Cleanup: CI & Upgrade
    - Upgraded Elixir to version 1.6
    - Added formatter configuration
    - Added Travis CI Configuration YAML file

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Text2Sign.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :text_2_sign,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :text_2_sign,
+      version: "0.1.1",
+      elixir: "~> 1.6",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -15,8 +17,6 @@ defmodule Text2Sign.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.9.0"},
-     {:floki, "~> 0.11.0"},
-     {:mock, "~> 0.2.0", only: :test}]
+    [{:httpoison, "~> 0.9.0"}, {:floki, "~> 0.11.0"}, {:mock, "~> 0.2.0", only: :test}]
   end
 end
